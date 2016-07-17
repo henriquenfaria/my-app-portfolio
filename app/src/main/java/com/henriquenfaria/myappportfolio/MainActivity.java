a87f59450b1502c43f51f3ee545d38a3a227f833
Henrique Faria <henriquenunesfaria@gmail.com>
2016-07-17 17:53:30 -0300

    Remove unused imports

--- app/src/main/java/com/henriquenfaria/myappportfolio/MainActivity.java
+++ app/src/main/java/com/henriquenfaria/myappportfolio/MainActivity.java
@@ -2,7 +2,6 @@ package com.henriquenfaria.myappportfolio;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.v4.app.FragmentManager;
--- app/src/main/java/com/henriquenfaria/myappportfolio/PortfolioFragment.java
+++ app/src/main/java/com/henriquenfaria/myappportfolio/PortfolioFragment.java
@@ -1,8 +1,6 @@
 package com.henriquenfaria.myappportfolio;
 
 import android.content.Context;
-import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
